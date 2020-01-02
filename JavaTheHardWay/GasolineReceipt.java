5e0b83d7e3dcbd751f234c1bbe2e49b32c74b2ee
sethschn <sethschn301@gmail.com>
2020-01-02 12:11:08 -0700

    Exercise 2

--- JavaTheHardWay/GasolineReceipt.java
+++ JavaTheHardWay/GasolineReceipt.java
@@ -4,12 +4,12 @@ public class GasolineReceipt{
     System.out.println("|                        |");
     System.out.println("|      CORNER STORE      |");
     System.out.println("|                        |");
-    System.out.println("| 2015-03-29 04:38PM     |");
+    System.out.println("| 2020-01-01 04:38PM     |");
     System.out.println("|                        |");
     System.out.println("| Gallons:        10.870 |");
-    System.out.println("| Price/gallon: $ 2.089  |");
+    System.out.println("| Price/gallon: $ 2.65   |");
     System.out.println("|                        |");
-    System.out.println("| Fuel total: $ 22.71    |");
+    System.out.println("| Fuel total: $ 28.80    |");
     System.out.println("|                        |");
     System.out.println("+------------------------+");
   }

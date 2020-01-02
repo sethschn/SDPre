01ee44c67fe4d359fd666ea493869cff160ce518
sethschn <sethschn301@gmail.com>
2020-01-02 12:07:22 -0700

    Exercise 1

--- JavaTheHardWay/FirstProg.java
+++ JavaTheHardWay/FirstProg.java
@@ -1,6 +1,6 @@
 public class FirstProg{
   public static void main(String[] args){
     System.out.println("I am determined that you will learn code. Seth");
-    System.out.println("Today is a great day Jan 1 2020");
+    System.out.println("Today's date is Jan 1 2020");
   }
 }

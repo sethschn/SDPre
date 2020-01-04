a6d6409ac64810528bec36abeccacd3603ffa5d4
sethschn <sethschn301@gmail.com>
2020-01-04 14:15:57 -0700

    Objective 2 labs

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,9 @@
+public class Objective2Lab3{
+  public static void main(String[] args){
+    String food1, food2, food3;
+    food1 = "pizza";
+    food2 = "cheeseburger";
+    food3 = "steak";
+    System.out.println( "My Favorite Foods Are: \n1. " + food1 + "\n2. " + food2 + "\n3. " + food3 );
+  }
+}
---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,13 @@
+public class Objective2Lab4 {
+  public static void main( String[] args){
+    double num1, num2, num3, average;
+
+    num1 = 3;
+    num2 = 12;
+    num3 = 15;
+
+    average = num1 + num2 + num3;
+    average = average / 3;
+    System.out.println(average);
+  }
+}
